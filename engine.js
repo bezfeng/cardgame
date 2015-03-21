990d39d59b833cc8e81547ecd1ec714263da1c81
Bezhou Feng <bezfeng@gmail.com>
2015-03-21 01:31:11 -0400

    added more rules to engine

--- engine.js
+++ engine.js
@@ -1,16 +1,27 @@
 var Card = require("./card.js");
 
+// Helper method
+function setPlayerLose(player) {
+  player.status = "lose";
+  if (player.hand[0]) {
+    this.discard.push(player.hand[0]);
+  }
+}
+
+// Engine object
 function Engine() {
   this.pack = this._shufflePack(this._createPack());
+  this.discard = [];
+  this.immunePlayers = [];
 }
 
 Engine.prototype._createPack = function() {
   var pack = [];
   
   var cardTypes = Card.types();
-  for (int i = 0; i < cardTypes.length; i++) {
+  for (var i = 0; i < cardTypes.length; i++) {
     var c = new Card(cardTypes[i]);
-    for (int i = 0; i < Card.countInDeck(c.value); i++) {
+    for (var i = 0; i < Card.countInDeck(c.value); i++) {
       pack.push(new Card(cardTypes[i]))
     }
   }
@@ -18,7 +29,6 @@ Engine.prototype._createPack = function() {
   return pack;
 }
 
-// Shuffles the pack - based on the Fisher-Yates algorithm
 Engine.prototype._shufflePack = function(pack) {
   var i = pack.length, j, tempi, tempj;
   if (i === 0) return false;
@@ -31,10 +41,89 @@ Engine.prototype._shufflePack = function(pack) {
 
 Engine.prototype.reset = function() {
   this.pack = this._shufflePack(this._createPack());
+  this.discard = [];
 }
 
-Engine.prototype.drawCard = function() {
-  return this.pack.pop();
+Engine.prototype.drawCard = function(player) {
+  var newCard = this.pack.pop();
+  player.hand.push(newCard);
+  return newCard;
+}
+
+Engine.prototype.play = function(targetPlayer, sourcePlayer, action) {
+  var card = action.card;
+  this.discard.push(card);
+  
+  // Remove the sourcePlayer from the immune list if they were previously on it.
+  // If targetPlayer is on the immune list, do nothing this turn.
+  var targetImmune = false;
+  for (var i = 0; i < this.immunePlayers.length; i++) {
+    var player = this.immunePlayers[i];
+    if (player.id == sourcePlayer.id) {
+      this.immunePlayers.splice(i, 1);
+    }
+    if (player.id == targetPlayer.id) {
+      targetImmune = true;
+    }
+  }
+  
+  if (targetImmune) {
+    return;
+  }
+  
+  // Guard - Guess the card
+  if (card.shortCode == "g") {
+    var guess = action.guess;
+    var targetPlayerCard = targetPlayer.hand[0];
+    if (guess == targetPlayerCard) {
+      targetPlayer.status = "lose";
+      this.discard.push(targetPlayerCard);
+    }
+  }
+  
+  // Courtier - Show sourcePlayer targetPlayer's cards
+  if (card.shortCode == "c") {
+    // ??
+  }
+  
+  // Diplomat - Compare hands, higher wins
+  if (card.shortCode == "d") {
+    var targetPlayerCard = targetPlayer.hand[0];
+    var sourcePlayerCard = sourcePlayer.hand[0];
+    
+    if (targetPlayerCard.value > sourcePlayerCard.value) {
+      setPlayerLose(sourcePlayer);
+    } else if (sourcePlayerCard.value < targetPlayerCard.value) {
+      setPlayerLose(targetPlayer);
+    }
+  }
+  
+  // Shugenja - invincible
+  if (card.shortCode == "s") {
+    sourcePlayer.status = "immune";
+    this.immunePlayers.push(sourcePlayer);
+  }
+  
+  // Hatamoto - targetPlayer drops all cards and picks a new one
+  if (card.shortCode == "h") {
+    while(targetPlayer.hand.length > 0) {
+      this.discard.push(targetPlayer.hand.pop());
+    }
+    targetPlayer.hand.push(this.pack.pop());
+  }
+  
+  // Manipulator - swap hands
+  if (card.shortCode == "m") {
+    var targetHand = targetPlayer.hand.slice();
+    targetPlayer.hand = sourcePlayer.hand;
+    sourcePlayer.hand = targetHand;
+  }
+  
+  // Princess - auto lose
+  if (card.shortCode == "p") {
+    setPlayerLose(sourcePlayer);
+  }
+  
 }
 
 module.exports = Engine;
