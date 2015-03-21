8d1c57086b4fc5810528fd409455857ba0b53261
Bezhou Feng <bezfeng@gmail.com>
2015-03-21 12:29:13 -0400

    added some more console logging and better messaging in the client textfield

--- player.js
+++ player.js
@@ -2,7 +2,7 @@ function Player(playerID, name){
   this.id = playerID;
   this.name = name;
   this.hand = [];
-  this.status;
+  this.status = "";
 }
 
 module.exports = Player;
--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var io = socket.listen(server);
 
 var table = new Table(1);
 
+console.log("Server up, listening on 8080!");
+
 app.get('/', function (req, res) {
   console.log("index.html get request")
   res.sendfile(__dirname + '/index.html');
@@ -29,7 +31,21 @@ io.sockets.on('connection', function (socket) {
     table.addPlayer(socket.id, data.playerName);
 
     console.log("Player in table " + JSON.stringify(table.players));
-    socket.emit("joinedTable", {message: "Joined Table: "+table.name+"\nHand: "+JSON.stringify(table.getPlayer(socket.id).hand)});
+    
+    var hand = table.getPlayer(socket.id).hand;
+    var handString = "";
+    for (var i = 0; i < hand.length; i++) {
+      handString = handString + hand[i].name + ", ";
+    }
+    
+    var playerList = "";
+    for (var i = 0; i < table.players.length; i++) {
+      playerList = playerList + table.players[i].name + ", ";
+    }
+    
+    socket.emit("joinedTable", {message: "Joined Table: " + table.name +
+                                         "\nPlayers: " + playerList + 
+                                         "\nHand: " + handString});
   });
 
 });
